316fb1bf42da3da9fc486c9941385891ea8ce06a
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-04-08 13:53:39 -0700

    [cleaned] up the factorial_iterative

--- challenges/recursion_and_searching/recursion.py
+++ challenges/recursion_and_searching/recursion.py
@@ -20,8 +20,8 @@ def factorial_iterative(n):
     result = 1
     if n == 0 or n == 1:
         return 1
-    for i in range(n):
-        result *= (i + 1)
+    for i in range(1, n + 1):
+        result *= i
     return result 
 
 def factorial_recursive(n):
