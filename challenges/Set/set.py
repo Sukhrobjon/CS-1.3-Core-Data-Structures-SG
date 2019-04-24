8413005b21856d1e3cacafe60da63d6b3e208a0d
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-04-24 00:13:22 -0700

    [modified] comments and not used functions

--- challenges/Set/set.py
+++ challenges/Set/set.py
@@ -10,7 +10,7 @@ class Set(object):
 
         # NOTE: This is called from _resize() method
         self.buckets = [LinkedList() for i in range(init_size)]
-        self.size = 0  # Number of key-value entries
+        self.size = 0  # Number of elements
 
     def __str__(self):
         """Return a formatted string representation of this hash table."""
@@ -21,34 +21,14 @@ class Set(object):
         """Return a string representation of this hash table."""
         return 'HashTable({!r})'.format(self.items())
 
-    def _bucket_index(self, key):
-        """Return the bucket index where the given key would be stored."""
-        return hash(key) % len(self.buckets)
+    def _bucket_index(self, element):
+        """Return the bucket index where the given element would be stored."""
+        return hash(element) % len(self.buckets)
 
     def load_factor(self):
         """Return the load factor, the ratio of number of entries to buckets.
         Best and worst case running time: ??? under what conditions? [TODO]"""
         return self.size / len(self.buckets)
-
-    # def keys(self):
-    #     """Return a list of all keys in this hash table.
-    #     Best and worst case running time: ??? under what conditions? [TODO]"""
-    #     # Collect all keys in each of the buckets
-    #     all_keys = []
-    #     for bucket in self.buckets:
-    #         for key, value in bucket.items():
-    #             all_keys.append(key)
-    #     return all_keys
-
-    # def values(self):
-    #     """Return a list of all values in this hash table.
-    #     Best and worst case running time: ??? under what conditions? [TODO]"""
-    #     # Collect all values in each of the buckets
-    #     all_values = []
-    #     for bucket in self.buckets:
-    #         for key, value in bucket.items():
-    #             all_values.append(value)
-    #     return all_values
 
     def items(self):
         """Return a list of all entries (key-value pairs) in this hash table.
@@ -85,22 +65,22 @@ class Set(object):
         # Check if an entry with the given key exists in that bucket
         entry = bucket.find(element)
         return entry is not None # True or False
-    # def get(self, key):
-    #     """Return the value associated with the given key, or raise KeyError.
-    #     Best case running time: ??? under what conditions? [TODO]
-    #     Worst case running time: ??? under what conditions? [TODO]"""
-    #     # Find the bucket the given key belongs in
-    #     index = self._bucket_index(key)
-    #     bucket = self.buckets[index]
-    #     # Find the entry with the given key in that bucket, if one exists
-    #     entry = bucket.find(lambda key_value: key_value[0] == key)
-    #     if entry is not None:  # Found
-    #         # Return the given key's associated value
-    #         assert isinstance(entry, tuple)
-    #         assert len(entry) == 2
-    #         return entry[1]
-    #     else:  # Not found
-    #         raise KeyError('Key not found: {}'.format(key))
+    def get(self, element):
+        """Return the value associated with the given key, or raise KeyError.
+        Best case running time: ??? under what conditions? [TODO]
+        Worst case running time: ??? under what conditions? [TODO]"""
+        # Find the bucket the given key belongs in
+        index = self._bucket_index(key)
+        bucket = self.buckets[index]
+        # Find the entry with the given key in that bucket, if one exists
+        entry = bucket.find(lambda key_value: key_value[0] == key)
+        if entry is not None:  # Found
+            # Return the given key's associated value
+            assert isinstance(entry, tuple)
+            assert len(entry) == 2
+            return entry[1]
+        else:  # Not found
+            raise KeyError('Key not found: {}'.format(key))
 
     def add(self, element):
         """Insert new element.
